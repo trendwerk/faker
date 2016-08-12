60cbc065dcebd7b441661c7547a44295a8e7fbe0
Harold Angenent <harold@trendwerk.nl>
2016-08-12 14:49:07 +0200

    Support all WordPress properties

--- src/Post.php
+++ src/Post.php
@@ -5,12 +5,27 @@ use Nelmio\Alice\Fixtures\Loader;
 
 final class Post
 {
+    public $comment_status;
+    public $guid;
+    public $menu_order;
     public $meta;
+    public $ping_status;
     public $post_author = 1;
     public $post_content;
+    public $post_content_filtered;
+    public $post_date;
+    public $post_date_gmt;
+    public $post_excerpt;
+    public $post_mime_type;
+    public $post_modified;
+    public $post_modified_gmt;
+    public $post_name;
+    public $post_parent;
+    public $post_password;
     public $post_status = 'publish';
     public $post_title;
     public $post_type;
+    public $to_ping;
 
     public function getPostData()
     {
@@ -18,7 +33,7 @@ final class Post
 
         unset($data['meta']);
 
-        return $data;
+        return array_filter($data);
     }
 
     public function getMeta()
