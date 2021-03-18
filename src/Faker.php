861361dda8f8ed6be26ec5e1cb1184910187694a
Jan Henk Hazelaar <janhenk@concept7.nl>
2021-03-18 10:38:29 +0100

    Using AliceDataFixtures

--- src/Faker.php
+++ src/Faker.php
@@ -1,7 +1,8 @@
 <?php
 namespace Trendwerk\Faker;
 
-use Nelmio\Alice\Fixtures\Loader;
+// use Nelmio\Alice\Fixtures\Loader;
+use Nelmio\Alice\Loader\NativeLoader;
 
 final class Faker
 {
@@ -11,7 +12,7 @@ final class Faker
     public function __construct($files)
     {
         $this->files = $files;
-        $this->loader = new Loader(get_locale(), [new Provider\Term]);
+        $this->loader = new NativeLoader();
     }
 
     public function persist(ProgressBar $progressBar)
@@ -19,7 +20,7 @@ final class Faker
         $persister = new Persister($progressBar);
 
         foreach ($this->files as $file) {
-            $objects = $this->loader->load($file);
+            $objects = $this->loader->loadFile($file);
             $persister->persist($objects);
         }
     }
@@ -29,8 +30,8 @@ final class Faker
         $objects = [];
 
         foreach ($this->files as $file) {
-            $set = $this->loader->load($file);
-            $objects = array_merge($objects, $set);
+            $set = $this->loader->loadFile($file);
+            $objects = array_merge($objects, $set->getObjects());
         }
 
         return count($objects);
